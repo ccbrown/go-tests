48bea64455ba44e87acdc28241c5af9478e4bc55
Christopher Brown <ccbrown112@gmail.com>
2020-11-05 01:56:00 -0500

    kljasldjqw

--- linux/dylib/lib.cpp
+++ linux/dylib/lib.cpp
@@ -1,6 +1,6 @@
 //go:cgo_import_dynamic _ _ "./dylib/libfoo.so"
-//go:cgo_ldflag "-Wl,-plugin=./plugin/plugin.so"
+//go:cgo_ldflag "-Wl,-plugin=/go/src/github.com/ccbrown/go-tests/linux/plugin/plugin.so"
 //_ _ "./dylib/libfoo.so"
-//go:cgo_ldflag "-Wl,-plugin=./plugin/plugin.so"
+//go:cgo_ldflag "-Wl,-plugin=/go/src/github.com/ccbrown/go-tests/linux/plugin/plugin.so"
 //""
-extern "C" void _____QDSdylibSlibfooDsoQ_SSgoCcgo_ldflag_QHWlCHpluginEDSpluginSpluginDsoQ_SS() {}
+extern "C" void _____QDSdylibSlibfooDsoQ_SSgoCcgo_ldflag_QHWlCHpluginESgoSsrcSgithubDcomSccbrownSgoHtestsSlinuxSpluginSpluginDsoQ_SS() {}
